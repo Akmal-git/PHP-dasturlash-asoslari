03aabb49a3d5f8b96ae152774613e5e85e9b3218
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-10-24 01:10:35 +0500

    php birinchi kod

--- xmpp.php
+++ xmpp.php
@@ -1,2 +1,6 @@
-<!-- XAMPP ni ornatib olamiz -->
+<!-- XAMPP ni yuklab olamiz olamiz -->
 <!-- XAMPP + Apache + MariaDB + PHP + Perl -->
+<!-- Terminalga yoziladi: -->
+<!-- php -v   => versiyasini bilish -->
+<!-- which php   =>  qaysi papkada turganini -->
+<?php echo 'Hello World' ?>
