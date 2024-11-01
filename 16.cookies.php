7ba27618b9c21e92c2b23ad76ef2eda51cbcc4a5
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-11-01 07:05:01 +0500

    Cookies

--- 16.cookies.php
+++ 16.cookies.php
@@ -1,4 +1,9 @@
-<!-- Browzreimizda saqlanadigan fayillar -->
+<!-- Browzreimizda saqlanadigan fayillar 
+Cookie ko'pincha foydalanuvchini aniqlash uchun ishlatiladi.
+Cookie - bu server foydalanuvchi kompyuteriga joylashtirgan kichik fayl. 
+Har safar bir xil kompyuter brauzerli sahifani so'raganda, u cookie-faylni ham yuboradi.
+PHP yordamida siz ham cookie qiymatlarini yaratishingiz va ham olishingiz mumkin. 
+-->
 
 <?php
 setcookie("nomi", "qiymmati");
@@ -21,6 +26,29 @@ var_dump($_COOKIE)
     <div>
         <h1><?php echo $_COOKIE['user_name'] ?></h1>
     </div>
+</body>
+
+</html>
+
+
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+<html>
+
+<body>
+
+    <?php
+    if (!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+        echo "Cookie '" . $cookie_name . "' is set!<br>";
+        echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+    ?>
+
 </body>
 
 </html>
